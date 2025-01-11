94a1ea04155cc3f8866a6b74abcaffecdac9efe3
Hrutam <hrutamsabale@gmail.com>
2025-01-11 13:23:06 +0530

    Added Templates

--- Section 02 - Essential Concepts of C and C++/Template Classes/cpp-main0.cpp
+++ Section 02 - Essential Concepts of C and C++/Template Classes/cpp-main0.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
+class Arithmatic
+{
+    private:
+    int a;
+    int b;
+
+    public:
+    Arithmatic(int a, int b);
+    void PrintAll();
+};
+
+Arithmatic::Arithmatic(int a, int b)
+{
+    this->a = a;
+    this->b = b;
+}
+
+void Arithmatic::PrintAll()
+{
+    cout<<"a: "<<this->a<<" b: "<<this->b;
+}
 int main()
 {
-cout<<"Test";
+    Arithmatic test(1,2);
+    test.PrintAll();
 }
